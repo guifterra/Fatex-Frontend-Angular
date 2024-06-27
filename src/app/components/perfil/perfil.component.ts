cf23b52b2c13bf8b6506ef3b9f4911a0276f2c24
Guilherme Terra <guilhermeterra391@gmail.com>
2024-06-26 22:46:13 -0300

    Tornarme motorista

--- src/app/components/perfil/perfil.component.ts
+++ src/app/components/perfil/perfil.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { Usuario } from '../../modelo/Usuario';
+import { UsuarioService } from '../../services/USU_USUARIO/usuario.service';
+import { MotoristaService } from '../../services/MOT_MOTORISTAS/motorista.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-perfil',
@@ -10,4 +14,25 @@ import { ButtonModule } from 'primeng/button';
 })
 export class PerfilComponent {
 
+  currentUser: Usuario = new Usuario();
+
+  constructor(
+    private servico: UsuarioService,
+    private motoristaService: MotoristaService,
+    private router: Router
+  ) {
+
+    if(!((this.servico.getCurrentUser()) == null))
+      this.currentUser = this.servico.getCurrentUser() ?? new Usuario(); // Obter dados do usuário do Local Storage
+    else
+      this.currentUser = new Usuario();
+  }
+
+  tornarMeMotorista(){
+    
+    const result = this.motoristaService.tornarSeMotorista();
+
+    if( result )
+    this.router.navigate(['/']);
+  }
 }
